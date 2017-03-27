63fd470ea0aad9f506f12d71a354fa9ba74f7a02
Nick Manning <Man@Nicks-MacBook-Pro.local>
2017-03-27 12:39:21 -0400

    Remove deprecated subModels/subCollections hash
    and that which is associated with it

--- Model.js
+++ Model.js
@@ -9,9 +9,7 @@ export default Backbone.Model.extend({
       this.query = new URLSearchParams(window.location.search);
     }
 
-    //possibly deprecated because if subModels and subCollections are separated then how to you ref them in get id there is a name conflict?
-    this.subModels = {};
-    this.subCollections = {};
+   
 
     //new
     this.structure = {};
@@ -20,36 +18,26 @@ export default Backbone.Model.extend({
     this.init();
   },
   init:function(){},
-  registerSubCollection:function(prop,collection){
-    if (_.isArray(collection)) collection = new Base.Collection(collection);
-    else if (!(collection instanceof Backbone.Collection)) collection = new Base.Collection(_.toArray(collection))
-    this.subCollections[prop] = collection;
-    collection.parentModels.push(this);
-    this.listenTo(collection,"add remove reset sort",function(){
-      this.trigger("change");
-    })
-  },
+  
   get:function(attr){
-    if (_.isString(attr) && attr.startsWith("->")) {
-      return this.structure[attr.substr(2)];
+
+    if (_.isString(attr)){
+      var props = attr.split("->");
+      if (props.length > 1){
+        var model = this;
+        props.slice(1).forEach(function(prop){
+          if (model.structure[prop]) model = model.structure[prop];
+        });
+        return model;
+      }
     }
-    else{
+      else{
       var get = Backbone.Model.prototype.get.apply(this,arguments);
       if (!_.isUndefined(get)) return get;
-
-      var props = attr.split(".");
-        if (props.length > 1){
-          var model = this;
-          props.forEach(function(prop){
-            if (model.subModels[prop]) model = model.subModels[prop];
-            else if (model.subCollections[prop]) model = model.subCollections[prop]
-          })
-          return model;   
-        }
-    
-        return this.subModels[attr] || this.subCollections[attr]
     }
+    
 
+ 
    
    
   },
